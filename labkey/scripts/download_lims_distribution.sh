98178beca4640c2873dcb74d4f20c5f930fa1e7d
Brandon Kieft <bkieft@lbl.gov>
2024-06-13 11:35:34 -0700

    Add functionality for customizing script to pull LabKey from source

--- labkey/scripts/download_lims_distribution.sh
+++ labkey/scripts/download_lims_distribution.sh
@@ -8,13 +8,12 @@ fi
 
 # Construct the URL and define the location to save the file
 url="https://lk-binaries.s3-us-west-2.amazonaws.com/downloads/release/community/${1%%-*}/LabKey$1-community-embedded.tar.gz"
-location=$2/src/$(basename $url)
+location=$2/src/latest.tar.gz
 
 # Download the file
 curl -o $location --create-dirs $url
 
 # Move to the labkey repo directory and extract jar
-tar xzvf $location --include='labkeyServer.jar' --include='VERSION' --strip-components 1 -C $2
-
-# Remove the tar file
-rm $location
+tar xzvf $2/src/latest.tar.gz -C $2/src/
+cp ./src/*$2*/labkeyServer.jar ./
+cp ./src/*$2*/VERSION ./
