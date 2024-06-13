b22f12fdd631f89f066b95373055c4ee2c6b566e
Brandon Kieft <bkieft@lbl.gov>
2024-06-13 15:04:05 -0700

    Try to fix permissions error in Makefile commands

--- labkey/make_command.sh
+++ labkey/make_command.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-make LABKEY_VERSION=24.3.4-6 DEBUG=1 NEW_DOWNLOAD=1 LABKEY_BUILD_DATE=$(date "+%Y-%m-%d-%H-%M") all
+make LABKEY_VERSION=24.3.4-6 NEW_DOWNLOAD=0 LABKEY_BUILD_DATE=$(date "+%Y-%m-%d-%H-%M") all
