d82599dc244713c684a870152bc53429235650b3
Brandon Kieft <bkieft@lbl.gov>
2024-07-26 15:49:30 -0700

    Overhaul repo for new build/deploy routine based on LabKey v24 distro

--- labkey/update_lock.sh
+++ labkey/update_lock.sh
@@ -8,4 +8,4 @@ if [ "$#" -ne 1 ]; then
 fi
 
 docker run -it --rm -v "${script_dir}:/tmp" -w '/tmp' "doejgi/conda-lock:latest" \
-     conda-lock -f "$1" -p linux-64 --lockfile "${1%.yaml}-lock.yaml"
+     conda-lock -f "$1" -p linux-64 -p linux-aarch64 --lockfile "${1%.yaml}-lock.yaml"
